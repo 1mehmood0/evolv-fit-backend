7eae25bcec318ffc62917fc90f878b01fe0a0285
1pervysage0 <msyed7008@gmail.com>
2022-03-13 05:29:39 +0530

    Level 3 done

--- routes/session.js
+++ routes/session.js
@@ -64,7 +64,7 @@ router.post("/create",async(req,res)=>{
     }
 })
 
-//UPDATE (LVL-2B)
+//UPDATE (LVL-2B) with using the id of session and can be removed if want to
 router.put("/update/:id",async(req,res)=>{
     try{
         const filter={_id:req.params.id};
@@ -82,6 +82,36 @@ router.put("/update/:id",async(req,res)=>{
         res.status(500).send(e);
     }
 });
+
+
+//UPDATE DATE in the SESSION COLLECTION with userRef._id if (isCompleted=False)
+router.put("/update/date/:id",async(req,res)=>{
+    const filter=req.params.id;
+    const checkDate=req.body.date;
+    console.log(checkDate,filter)
+    try{
+        const fetchedSession=await Session.find({isCompleted:false});
+        console.log(fetchedSession);
+       const upDate=fetchedSession.map((x)=>{
+
+            let dateMs=new Date(x.date).getTime()+86400000;
+            x.date=new Date(dateMs).toISOString();
+
+           Session.findByIdAndUpdate(x._id,{$set:{date:x.date}},(err,data)=>{
+                if(err)
+                console.log(err,"NOTsaved to DB new DATE");
+            });
+            return x;
+        });
+        // await fetchedSession.save();
+         console.log(upDate,"<----UPDATED DATE"); 
+        res.send(upDate);
+    }
+    catch(e){
+        res.status(500).send(e);
+    }
+
+})
 
 router.get("/:id",async(req,res)=>{
    const getSession=await Session.findById(req.params.id).populate("trainerRef").populate("userRef").populate("trainerRef");
